7b4ceb3a85182de6a1af40ec9b70bd0c1b666e07
AJ Christensen <aj@junglist.io>
2013-07-27 23:22:58 +1200

    continubus: fuck it, add opt too

--- continubus/dependencies.sh
+++ continubus/dependencies.sh
@@ -5,3 +5,4 @@ apt-get install ruby1.9.1-dev rubygems build-essential libxslt1-dev libxml2-dev 
 gem1.9.1 install bundler
 mkdir -p /var/cache/omnibus
 chown -R go: /var/cache/omnibus
+chown -R go: /opt
