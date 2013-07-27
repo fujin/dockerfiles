4eebaa0eb7f2b1802812274522236821a04b2896
AJ Christensen <aj@junglist.io>
2013-07-27 22:44:32 +1200

    continubus: try regular ruby plz

--- continubus/dependencies.sh
+++ continubus/dependencies.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 apt-get update
-apt-get install python-software-properties -y
-apt-add-repository ppa:brightbox/ruby-ng
-apt-get update
-apt-get install ruby ruby-dev rubygems ruby1.9.3 ruby-switch build-essential libxslt1-dev libxml2-dev unzip git-core -y
-ruby-switch --set ruby1.9.1
-gem install bundler
+apt-get install ruby1.9.1-dev rubygems build-essential libxslt1-dev libxml2-dev unzip git-core -y
+gem1.9.1 install bundler
