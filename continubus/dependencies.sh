c705cbaf14ef4658902cfb86fedb3d0bb6f557e2
AJ Christensen <aj@junglist.io>
2013-07-27 22:14:10 +1200

    continubus: add ruby dev so mkmf works

--- continubus/dependencies.sh
+++ continubus/dependencies.sh
@@ -4,6 +4,6 @@ apt-get update
 apt-get install python-software-properties -y
 apt-add-repository ppa:brightbox/ruby-ng
 apt-get update
-apt-get install ruby rubygems ruby1.9.3 ruby-switch build-essential libxslt1-dev libxml2-dev unzip git-core -y
+apt-get install ruby ruby-dev rubygems ruby1.9.3 ruby-switch build-essential libxslt1-dev libxml2-dev unzip git-core -y
 ruby-switch --set ruby1.9.1
 gem install bundler
