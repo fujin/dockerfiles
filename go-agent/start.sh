20e80c25f04817789f9e64721cd60373ed638517
AJ Christensen <aj@junglist.io>
2013-11-15 11:10:11 +1300

    rip out hard-coded default settings, echo into the file

--- go-agent/start.sh
+++ go-agent/start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 . /etc/default/go-agent
 chown -R go: /tmp
+
 echo 127.0.0.1 $(hostname) > /etc/hosts
-echo "expects link alias 'go_server', echoing"
+
+echo expects link alias 'go_server', echoing values into /etc/default/go-agent
 env | grep GO_SERVER
-echo "starting go-agent"
-export GO_SERVER=${GO_SERVER_PORT_8153_TCP_ADDR}
-export GO_SERVER_PORT=${GO_SERVER_PORT_8153_TCP_PORT}
+echo starting go-agent
+
+echo GO_SERVER=${GO_SERVER_PORT_8153_TCP_ADDR} >> /etc/default/go-agent
+echo GO_SERVER_PORT=${GO_SERVER_PORT_8153_TCP_PORT} >> /etc/default/go-agent
 
 /bin/su - go -c /usr/share/go-agent/agent.sh
