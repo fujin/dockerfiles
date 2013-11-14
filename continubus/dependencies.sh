6d05c977a6d6d4feeb570e32cc4d32cc64438266
AJ Christensen <aj@junglist.io>
2013-11-15 12:56:57 +1300

    add universe/multiverse

--- continubus/dependencies.sh
+++ continubus/dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo deb http://archive.ubuntu.com/ubuntu precise main universe multiverse > /etc/apt/sources.list
 apt-get update
 apt-get install ruby1.9.1-dev rubygems build-essential libxslt1-dev libxml2-dev unzip git-core automake libtool -y
 gem1.9.1 install bundler
