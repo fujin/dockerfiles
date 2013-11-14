a33c05a6f1a9f701d091a2c6da104884e37cf755
AJ Christensen <aj@junglist.io>
2013-11-15 11:06:33 +1300

    party time with go-agent and links

--- go-agent/start.sh
+++ go-agent/start.sh
@@ -2,4 +2,10 @@
 . /etc/default/go-agent
 chown -R go: /tmp
 echo 127.0.0.1 $(hostname) > /etc/hosts
+echo "expects link alias 'go_server', echoing"
+env | grep GO_SERVER
+echo "starting go-agent"
+export GO_SERVER=${GO_SERVER_PORT_8153_TCP_ADDR}
+export GO_SERVER_PORT=${GO_SERVER_PORT_8153_TCP_PORT}
+
 /bin/su - go -c /usr/share/go-agent/agent.sh
